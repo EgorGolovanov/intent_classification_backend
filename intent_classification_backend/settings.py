8a1670b734123388cfa1e59a41b7e808e72e7b06
EgorGolovanov <Egor_Golovanov_Kirov@users.noreply.github.com>
2019-12-24 11:38:17 +0300

    Update settings.py

--- intent_classification_backend/settings.py
+++ intent_classification_backend/settings.py
@@ -29,7 +29,9 @@ SECRET_KEY = 'z2ef&=i9-y+@30-hmswr=n5xitl4n$x@+u8fw9&c_^cr8kuf^4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['intent-classification-app.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['intent-classification-app.herokuapp.com',
+                 'backend-practice-app.herokuapp.com',
+                 '127.0.0.1']
 
 
 # Application definition
